1987240e3fd08312557d2029f44b20079f2a5548
Katie <kati3.ed@gmail.com>
2025-12-04 07:43:06 +0100

    bless my wonderful chungus life, I think it works on forge now too

--- forge/src/main/java/link/e4mc/forge/E4mcClientForge.java
+++ forge/src/main/java/link/e4mc/forge/E4mcClientForge.java
@@ -1,16 +1,16 @@
 package link.e4mc.forge;
 
 import link.e4mc.E4mcClient;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
 
 @Mod(E4mcClient.MOD_ID)
 public class E4mcClientForge {
     public E4mcClientForge() {
         E4mcClient.init();
-        MinecraftForge.EVENT_BUS.register(this);
+        RegisterCommandsEvent.BUS.addListener(this::onRegisterCommandEvent);
     }
 
     @SubscribeEvent
